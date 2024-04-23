65e4c6af1b40db790101ba89aa83142a817ccdb7
tarikkoval99 <tarikkoval99@gmail.com>
2024-04-23 19:17:39 +0200

    Initial Commit

---
+++ TrainingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrainingApp
+//
+//  Created by Тарас Коваль on 23.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
